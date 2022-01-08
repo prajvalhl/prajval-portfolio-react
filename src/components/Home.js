73612969cb7a8909b477d04a2a6db52c8990df54
prajvalhl <prajvalhl@gmail.com>
2022-01-08 12:14:27 +0530

    docs: updated resume link

--- src/components/Home.js
+++ src/components/Home.js
@@ -80,7 +80,7 @@ export function Home() {
           <p>Have a look at my Resume!</p>
           <a
             className={`link link-primary ${themePalette.primary}`}
-            href="https://drive.google.com/uc?id=1_DLnihId2TYSLqnVvJmUDUF2J5zIhnEi&export=download"
+            href="https://drive.google.com/uc?id=1Qdm8kCrwqQB_nYbr-OPKGcZkU844h___&export=download"
             rel="noreferrer"
           >
             Resume
